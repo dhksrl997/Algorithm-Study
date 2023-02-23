cd5d58268c2df7a34448852ba50f38aa1687c2d4
Wan Gi Jeon <dhksrl997@gmail.com>
2023-02-24 00:20:59 +0900

    백준 2018

---
+++ src/main/java/BaekJoon/Silver5/BaekJoon2018.java
@@ -0,0 +1,52 @@
+package BaekJoon.Silver5;
+/*
+Created on 2023/02/21 1:21 AM In Intelli J IDEA 
+by jeon-wangi
+*/
+
+import java.util.Arrays;
+
+//https://www.acmicpc.net/problem/2018
+public class BaekJoon2018 {
+    public static void main(String[] args) {
+        int n = 15;
+        System.out.println(new BaekJoon2018().twoPointer(15));
+    }
+/*
+    어떠한 자연수 N은, 몇 개의 연속된 자연수의 합으로 나타낼 수 있다.
+    당신은 어떤 자연수 N(1 ≤ N ≤ 10,000,000)에 대해서,
+    이 N을 몇 개의 연속된 자연수의 합으로 나타내는 가지수를 알고 싶어한다. 이때, 사용하는 자연수는 N이하여야 한다.
+    예를 들어, 15를 나타내는 방법은 15, 7+8, 4+5+6, 1+2+3+4+5의 4가지가 있다. 반면에 10을 나타내는 방법은 10, 1+2+3+4의 2가지가 있다.
+    N을 입력받아 가지수를 출력하는 프로그램을 작성하시오.
+*/
+
+    public int twoPointer(final int n) {
+        int[] array = new int[n];
+        init(array, n);
+        int sum = 1;
+        int count = 1;
+        int start = 1;
+        int end = 1;
+
+        while (end != n) {
+            if (sum == n) {
+                count++;
+                end++;
+                sum += end;
+            } else if (sum > n) {
+                sum -= start;
+                start++;
+            } else {
+                end++;
+                sum += end;
+            }
+        }
+
+        return count;
+    }
+
+    public void init(int[] array, int n) {
+        for (int i = 0; i < n; i++)
+            array[i] = i + 1;
+    }
+}
